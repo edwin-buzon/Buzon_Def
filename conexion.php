26b2d4a3aa4dec266c7303cefb1af4a8dc278447
edwin-buzon <44455742+edwin-buzon@users.noreply.github.com>
2018-10-29 12:22:33 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 	try {
-	$conexion = new PDO('mysql:host=localhost; dbname=sesion', 'root', ''); 
+	$conexion = new PDO('mysql:host=ec2-23-23-80-20.compute-1.amazonaws.com; dbname='dbklflrv2svhm0', 'ddnevyqejzwovs', '56e88593f53d4bc88f19e95bd92ac95a8e05918939246126739ea266c0374115'); 
 	$conexion ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$conexion->exec("SET CHARACTER SET utf8");
 
